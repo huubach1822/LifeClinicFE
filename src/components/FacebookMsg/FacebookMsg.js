661116e8d1387e4749560d4c3100292c52401da5
huubach1822 <monsterxyz02@gmail.com>
2024-05-11 23:00:06 +0700

    update2

--- src/components/FacebookMsg/FacebookMsg.js
+++ src/components/FacebookMsg/FacebookMsg.js
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 const FacebookMsg = () => {
 
     return (
-        <FacebookProvider appId="1002577934754132" chatSupport>
+        <FacebookProvider appId="3741911936085473" chatSupport>
             <CustomChat pageId="314203795110867" minimized={"true"} />
         </FacebookProvider>
     )
